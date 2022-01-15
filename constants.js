d900e84b81bbb68dcd69726fcd08fc7c3145cf46
sai1nanda <69192974+sai1nanda@users.noreply.github.com>
2022-01-15 11:25:52 +0530

    Create constants.js

---
+++ constants.js
@@ -0,0 +1,137 @@
+
+const tableData = [
+	{
+		topic : "Revenues" , 
+		"Y-1" : 0 , 
+		"Y" : 0 , 
+		"Y+1" : 0 , 
+		"Y+2" : 0 
+	},
+	{
+		topic : "Cost of Goods" , 
+		"Y-1" : 0 , 
+		"Y" : 0 , 
+		"Y+1" : 0 , 
+		"Y+2" : 0 
+	},
+	{
+		topic : "Salaries" , 
+		"Y-1" : 0 , 
+		"Y" : 0 , 
+		"Y+1" : 0 , 
+		"Y+2" : 0 
+	},
+	{
+		topic : "Operation Cost" , 
+		"Y-1" : 0 , 
+		"Y" : 0 , 
+		"Y+1" : 0 , 
+		"Y+2" : 0 
+	},
+	{
+		topic : "EBITDA" , 
+		"Y-1" : 0 , 
+		"Y" : 0 , 
+		"Y+1" : 0 , 
+		"Y+2" : 0 
+	},
+	{
+		topic : "D&A" , 
+		"Y-1" : 0 , 
+		"Y" : 0 , 
+		"Y+1" : 0 , 
+		"Y+2" : 0 
+	},
+	{
+		topic : "EBIT" , 
+		"Y-1" : 0 , 
+		"Y" : 0 , 
+		"Y+1" : 0 , 
+		"Y+2" : 0 
+	},
+	{
+		topic : "Interest" , 
+		"Y-1" : 0 , 
+		"Y" : 0 , 
+		"Y+1" : 0 , 
+		"Y+2" : 0 
+	},
+	{
+		topic : "Taxes" , 
+		"Y-1" : 0 , 
+		"Y" : 0 , 
+		"Y+1" : 0 , 
+		"Y+2" : 0 
+	},
+	{
+		topic : "Net Profit" , 
+		"Y-1" : 0 , 
+		"Y" : 0 , 
+		"Y+1" : 0 , 
+		"Y+2" : 0 
+	},
+	{
+		topic : "Recieveables" , 
+		"Y-1" : 0 , 
+		"Y" : 0 , 
+		"Y+1" : 0 , 
+		"Y+2" : 0 
+	},
+	{
+		topic : "Inventory" , 
+		"Y-1" : 0 , 
+		"Y" : 0 , 
+		"Y+1" : 0 , 
+		"Y+2" : 0 
+	},
+	{
+		topic : "Payables" , 
+		"Y-1" : 0 , 
+		"Y" : 0 , 
+		"Y+1" : 0 , 
+		"Y+2" : 0 
+	},
+	{
+		topic : "Working Capital" , 
+		"Y-1" : 0 , 
+		"Y" : 0 , 
+		"Y+1" : 0 , 
+		"Y+2" : 0 
+	},
+	{
+		topic : "Capital Expenditures" , 
+		"Y-1" : 0 , 
+		"Y" : 0 , 
+		"Y+1" : 0 , 
+		"Y+2" : 0 
+	},
+	{
+		topic : "Debt at the end of the year" , 
+		"Y-1" : 0 , 
+		"Y" : 0 , 
+		"Y+1" : 0 , 
+		"Y+2" : 0 
+	},
+	{
+		topic : "Change in outstanding debt" , 
+		"Y-1" : 0 , 
+		"Y" : 0 , 
+		"Y+1" : 0 , 
+		"Y+2" : 0 
+	},
+	{
+		topic : "Future Funding" , 
+		"Y-1" : 0 , 
+		"Y" : 0 , 
+		"Y+1" : 0 , 
+		"Y+2" : 0 
+	},
+	{
+		topic : "Free Cash Flow" , 
+		"Y-1" : 0 , 
+		"Y" : 0 , 
+		"Y+1" : 0 , 
+		"Y+2" : 0 
+	}
+]
+module.exports = tableData ; 
